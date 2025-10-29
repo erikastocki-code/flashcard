c69d9bb00af90d6425097b97ea4897f15077abc2
erikastocki-code <erika.stocki@escola.pr.gov.br>
2025-10-29 16:10:05 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,11 +1,17 @@
 criaCartao(
-  'Programação',
-  'O que é phyton?',
-' O Phyton é uma linguagem de programação'
-)
+    'Programação',
+    'O que é phyton?',
+  ' O Phyton é uma linguagem de programação'
+  )
+  
+  criaCartao(
+    'Geografia',
+    ' Qual a capital da França?',
+  ' Paris.'
+  )
 
-criaCartao(
-  'Geografia',
-  ' Qual a capital da França?',
-' Paris.'
-)
+  criaCartao(
+    'matemática',
+    ' Qual é a raiz quadrada de 64?',
+  ' a raiz é 8.'
+  )
